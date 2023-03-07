19b8bdca35d005f1b05dba8533d08bfe93e4ba92
Raghav <iraghav.me@gmail.com>
2023-03-06 20:43:50 -0500

    Outlined heuristic evaluation

--- src/ai.py
+++ src/ai.py
@@ -8,3 +8,8 @@ class Ai:
         # params: board (includes child elements), alpha, beta, depth = 3, player (white or black)
         # For each board loop through the child boards and get a herustic evalution (get number of points on board) and alternative between min and max until you reach the current tree node for the board
         pass
+
+    def heuristic(self):
+        # params: board, maximizing color
+        # determine how good a position is for a slect player
+        pass
